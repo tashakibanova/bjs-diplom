34560aca05f666937ff2e8a3e1f1ef0b67b072fe
unknown <ov.nattie@gmail.com>
2023-09-30 14:29:05 +0500

    Финальные правки

--- public/homePage.js
+++ public/homePage.js
@@ -10,7 +10,7 @@ userLogoutButton.action = function () {
 
 //Получение информации о пользователе
 ApiConnector.current((response) => {
-    if (response.success === true) {
+    if (response.success) {
       ProfileWidget.showProfile(response.data); // Отображение данных профиля при успешном запросе
     }
   });
@@ -19,7 +19,7 @@ ApiConnector.current((response) => {
 const ratesBoard = new RatesBoard();
 function getCurrencyRates() {
     ApiConnector.getStocks((response) => {
-      if (response.success === true) {
+      if (response.success) {
         ratesBoard.clearTable(); // Очистка таблицы
         ratesBoard.fillTable(response.data); // Заполнение таблицы полученными данными
       }
@@ -34,70 +34,77 @@ const moneyManager = new MoneyManager();
 //пополнение баланса
 moneyManager.addMoneyCallback = function (data) {
     ApiConnector.addMoney(data, (response) => {
-      if (response.success === true) {
+      if (response.success) {
         ProfileWidget.showProfile(response.data); // Отображение новых данных о пользователе
-        moneyManager.setMessage(true, 'Успешно'); // Отображение сообщения об успехе
+        moneyManager.setMessage(true, 'Баланс пополнен'); // Отображение сообщения об успехе
+        setTimeout(() => { this.favoritesMessageBox.style.display = 'none'; }, 5000);
       } else {
-        moneyManager.setMessage(false,'Ошибка пополнения'); // Отображение сообщения об ошибке
+        moneyManager.setMessage(false, response.error); // Отображение сообщения об ошибке
       }
-    });
+    })
 }
 //конвертирование валюты
 moneyManager.conversionMoneyCallback = function (data) {
     ApiConnector.convertMoney(data, (response) => {
-      if (response.success === true) {
+      if (response.success) {
         ProfileWidget.showProfile(response.data); // Отображение новых данных о пользователе
-        moneyManager.setMessage(true, 'Успешно'); // Отображение сообщения об успехе
+        moneyManager.setMessage(true, 'Валюта конвертирована'); // Отображение сообщения об успехе
+        setTimeout(() => { this.favoritesMessageBox.style.display = 'none'; }, 5000);
       } else {
-        moneyManager.setMessage(false,'Ошибка пополнения'); // Отображение сообщения об ошибке
+        moneyManager.setMessage(false, response.error); // Отображение сообщения об ошибке
       }
-    });
+    })
   }
 //перевод валюты
 moneyManager.sendMoneyCallback = function (data) {
     ApiConnector.transferMoney(data, (response) => {
-      if (response.success === true) {
+      if (response.success) {
         ProfileWidget.showProfile(response.data); // Отображение новых данных о пользователе
-        moneyManager.setMessage(true, 'Успешно'); // Отображение сообщения об успехе
+        moneyManager.setMessage(true, 'Перевод осуществлен'); // Отображение сообщения об успехе
+        setTimeout(() => { this.favoritesMessageBox.style.display = 'none'; }, 5000);
       } else {
-        moneyManager.setMessage(false,'Ошибка пополнения'); // Отображение сообщения об ошибке
+        moneyManager.setMessage(false, response.error); // Отображение сообщения об ошибке
       }
-    });
+    })
 }
 
-//Работа с избранным - НЕ РАБОТАЕТ ВЫПАДАЮЩИЙ СПИСОК
+//Работа с избранным
 const favoritesWidget = new FavoritesWidget(); 
 
 ApiConnector.getFavorites((response) => {
-    if (response.success === true) {
+    if (response.success) {
         favoritesWidget.clearTable(); // Очистка текущего списка избранного
         favoritesWidget.fillTable(response.data); // Отрисовка полученных данных
         moneyManager.updateUsersList(response.data); // Заполнение выпадающего списка для перевода денег
     } else {
-      favoritesWidget.setMessage(false,'Ошибка');
+      favoritesWidget.setMessage(false, response.error);
     }
   })
-
+//Реализуйте добавления пользователя в список избранных:
 favoritesWidget.addUserCallback = function (data) {
     ApiConnector.addUserToFavorites(data, (response) => {
-      if (response.success === true) {
+      if (response.success) {
         favoritesWidget.clearTable(); // Очистка текущего списка избранного
         favoritesWidget.fillTable(response.data); // Отрисовка полученных данных
         moneyManager.updateUsersList(response.data); // Заполнение выпадающего списка для перевода денег
+        favoritesWidget.setMessage(true, 'Пользователь добавлен');
+        setTimeout(() => { this.favoritesMessageBox.style.display = 'none'; }, 5000);
       } else {
-        favoritesWidget.setMessage(false,'Ошибка'); // Отображение сообщения об ошибке
+      favoritesWidget.setMessage(false, response.error);
       }
-    });
+    })
 }
-
+//Реализуйте удаление пользователя из избранного
 favoritesWidget.removeUserCallback = function (data) {
     ApiConnector.removeUserFromFavorites(data, (response) => {
-      if (response.success === true) {
+      if (response.success) {
         favoritesWidget.clearTable(); // Очистка текущего списка избранного
         favoritesWidget.fillTable(response.data); // Отрисовка полученных данных
         moneyManager.updateUsersList(response.data); // Заполнение выпадающего списка для перевода денег
+        favoritesWidget.setMessage(true, 'Пользователь удален');
+        setTimeout(() => { this.favoritesMessageBox.style.display = 'none'; }, 5000);
       } else {
-        favoritesWidget.setMessage(false,'Ошибка'); // Отображение сообщения об ошибке
+      favoritesWidget.setMessage(false, response.error);
       }
-    });
+    })
 }
--- public/loginPage.js
+++ public/loginPage.js
@@ -6,23 +6,23 @@ userFormObject.loginFormCallback = function(data) {
   ApiConnector.login(data, response => {
     console.log(response); // проверяем, какой объект возвращает сервер
     // проверяем успешность запроса
-    if(response.success === true) {
+    if(response.success) {
       location.reload();
     } else {
-      userFormObject.setLoginErrorMessage('Ошибка авторизации');
-    };
-  });
-};
+      userFormObject.setLoginErrorMessage(response.error);
+    }
+  })
+}
 registerFormCallback.loginFormCallback = function(data) {
     ApiConnector.login(data, response => {
       console.log(response); // проверяем, какой объект возвращает сервер
       // проверяем успешность запроса
-      if(response.success === true) {
+      if(response.success) {
         location.reload();
       } else {
-        userFormObject.setRegisterErrorMessage('Ошибка регистрации');
-      };
-    });
-  };
+        userFormObject.setRegisterErrorMessage(response.error);
+      }
+    })
+  }
 
 
